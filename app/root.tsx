6327aca268fc5ac0abb53d0caceb0cd10fdf724f
Ilya Kushlianski <kushliansky@gmail.com>
2023-11-08 00:41:08 +0300

    First action and loader
    Signed-off-by: Ilya Kushlianski <ikushlianski@Ilyas-Mac-mini.local>

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,36 @@
+import { json, LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Form,
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { createEmptyContact, getContacts } from "./data";
+
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -36,16 +59,31 @@ export default function App() {
           </div>
           <nav>
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite ? <span>★</span> : null}
+                  </Link>
+                </li>
+              ))}
             </ul>
+            ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+            )
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
